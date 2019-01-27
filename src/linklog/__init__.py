72cc2c48b68408e97dbd98f0f8541e10fa70cf8f
Keith Gaughan <k@stereochro.me>
2019-01-27 04:44:13 +0000

    Cut down on the dependencies.

--- src/linklog/__init__.py
+++ src/linklog/__init__.py
@@ -1,9 +1,134 @@
-from flask import Flask
+import contextlib
+import io
+import os
+import sqlite3
+from xml.sax import saxutils
+
+from flask import Flask, g
 
 
 app = Flask(__name__)
 
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db_path = os.environ.get('LINKLOG_DB_PATH', 'db.sqlite')
+        db = g._database = sqlite3.connect(db_path)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+class XMLBuilder:
+    """
+    XML document builder. The code is purposely namespace ignorant: it's up to
+    user to supply appropriate `xmlns` attributes as needed.
+
+    >>> xml = XMLBuilder()
+    >>> with xml.within('root', xmlns='tag:talideon.com,2013:test'):
+    ...     xml += 'Before'
+    ...     with xml.within('leaf'):
+    ...         xml += 'Within'
+    ...     xml += 'After'
+    ...     xml.tag('leaf', 'Another')
+    >>> print xml.as_string()
+    <?xml version="1.0" encoding="utf-8"?>
+    <root xmlns="tag:talideon.com,2013:test">Before<leaf>Within</leaf>After<leaf>Another</leaf></root>
+    """
+
+    def __init__(self, out=None, encoding="utf-8"):
+        """
+        `out` should be a file-like object to write the document to. If none
+        is provided, a buffer is created.
+
+        Note that if you provide your own, `as_string()` will return `None`
+        as no other sensible value can be returned.
+        """
+        if out is None:
+            self.buffer = io.StringIO()
+            out = self.buffer
+        else:
+            self.buffer = None
+        self.generator = saxutils.XMLGenerator(out, encoding)
+        self.generator.startDocument()
+
+    @contextlib.contextmanager
+    def within(self, tag, **attrs):
+        """
+        Generates an element containing nested elements.
+        """
+        self.generator.startElement(tag, attrs)
+        yield
+        self.generator.endElement(tag)
+
+    def tag(self, tag, *values, **attrs):
+        """
+        Generates a simple element.
+        """
+        self.generator.startElement(tag, attrs)
+        for value in values:
+            self.generator.characters(value)
+        self.generator.endElement(tag)
+
+    def append(self, other):
+        """
+        Append the string to this document.
+        """
+        self.generator.characters(other)
+        return self
+
+    def as_string(self):
+        """
+        If using the built-in buffer, get its current contents.
+        """
+        if self.buffer is None:
+            return None
+        return self.buffer.getvalue()
+
+    def close(self):
+        """
+        If using the built-in buffer, clean it up.
+        """
+        if self.buffer is not None:
+            self.buffer.close()
+            self.buffer = None
+
+    # Shortcuts.
+    __iadd__ = append
+    __str__ = as_string
+
+
 @app.route("/")
-def hello():
+def latest():
     return "Hello World!"
+
+
+@app.route("/;add")
+def add_entry():
+    return "Hello World!"
+
+
+@app.route("/;feed")
+def feed():
+    return ""
+
+
+@app.route("/<string(length=4):year>-<string(length=2):month>")
+def archive(year, month):
+    return ""
+
+
+@app.route("/<int:entry_id>")
+def entry(entry_id):
+    return ""
+
+
+@app.route("/<int:entry_id>;edit")
+def edit_entry(entry_id):
+    return ""
