f0d7813c63c8d1e4e2c46c4d4156a8ebbfeda050
Keith Gaughan <k@stereochro.me>
2019-01-27 04:00:47 +0000

    Some initial code.

---
+++ schema/linklog.sql
@@ -0,0 +1,12 @@
+CREATE TABLE links (
+	id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	title  TEXT    NOT NULL,
+	link   TEXT    NULL     UNIQUE,
+	via    TEXT    NULL,
+	time_c TEXT    NOT NULL DEFAULT (DATETIME('now')),
+	time_m TEXT    NOT NULL DEFAULT (DATETIME('now')),
+	note   TEXT    NOT NULL
+);
+
+CREATE INDEX links_created ON links (time_c);
+CREATE INDEX links_modified ON links (time_m);
