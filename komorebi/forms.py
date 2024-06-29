fa1c676a54fb1bed3ecca4ccd56091ec27fdbe38
Keith Gaughan <k@stereochro.me>
2024-06-29 02:08:58 +0100

    Fix Werkzeug and HTML5 breakage
    The formatting changes are basically being forced on me by VSCode.

--- komorebi/forms.py
+++ komorebi/forms.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
-from wtforms.fields.html5 import URLField
+from wtforms import StringField, TextAreaField, URLField
 from wtforms.validators import DataRequired
 
 
