a9b2524a6881b8685a2dae52de8aeb02100191ee
Keith Gaughan <k@stereochro.me>
2025-12-23 19:33:42 +0000

    Set the default backend when testing to the null backend

--- src/komorebi/app.py
+++ src/komorebi/app.py
@@ -14,6 +14,7 @@ def create_app(*, testing: bool = False) -> Flask:
                 "SERVER_NAME": "example.com",
                 "APPLICATION_ROOT": "/site/",
                 "PREFERRED_URI_SCHEME": "http",
+                "CACHE_TYPE": "NullBackend",
             }
         )
     app.register_blueprint(blog.blog)
